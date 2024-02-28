9e4909edd54fbadb902ba0f75ae4ea0ecacb4ca3
Niaz Baharudeen <tuk05348@temple.edu>
2024-02-28 17:17:03 -0500

    Finished last function with TA's help. Understood how to do it using views, as?, and ?:.

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -36,32 +36,11 @@ class MainActivity : AppCompatActivity() {
         val median = if ((this.size % 2 == 0)) (this[this.size / 2] + this[(this.size - 1) / 2]) / 2 else (this[this.size / 2])
         this.average() < median
     }
-    /*private fun averageLessThanMedian(listOfNumbers: List<Double>) : Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
-
-        return avg < median
-    }*/
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
-
-        textView.text = collection[position].toString()
-
-        return textView
-    }
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context):View = (recycledView as? TextView ?: TextView(context).apply{
+        setPadding(5, 10, 10, 0)
+        textSize = 22f
+    }).apply{text=collection[position].toString()}
 
 }
